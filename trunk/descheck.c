c3c48bd114f8507dec41c759abba1fd7c42e5c4d
lucian.grijincu <lucian.grijincu@74f295f6-9b29-0410-8176-5df8344103bc>
2007-02-18 18:55:05 +0000

    added all the needed files from binutils. We'll build descheck starting from these sources.
    I've simplified Makefile (we're not using autoconf. When descheck becomes stable and all features are implemented, we'll add autoconf support).
    Added descheck.c: this will be the place where we shall add new code.
    
    objdump segfaulted if it was compiled against the system libbfd.a, libopcodes.a and libiberty.a. If we don't identify the issue at hand we'll add the sources for these two libraries to the repository. For now we'll use some precompiled compatible versions.

---
+++ trunk/descheck.c
@@ -0,0 +1,11 @@
+void display_file (char *filename, char *target);
+
+int main (int argc, char **argv);
+
+int main (int argc, char **argv)
+{
+	argc = 0;
+	argv = 0;
+	display_file("descheck.o", 0);
+	return 0;
+}
